f2aaaa427763df780849f290f7729bae056f6f96
Days-gone 123 <1328699220@qq.com>
2023-10-12 07:51:04 +0000

    temp1

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -13,7 +13,10 @@ fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     // value of 0 The Option output should gracefully handle cases where
     // time_of_day > 23.
     // TODO: Complete the function body - remember to return an Option!
-    ???
+    if time_of_day > 22 {
+        let ans:Option<u16> = Some(5);
+        ans
+    }
 }
 
 #[cfg(test)]
