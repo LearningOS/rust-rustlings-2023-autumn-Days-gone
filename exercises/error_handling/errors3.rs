134adcfd9036eabaac094f0dff3990791113db96
Daysgone <79004213+Days-gone@users.noreply.github.com>
2023-10-13 09:57:15 +0000

    temp2

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -7,7 +7,6 @@
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
@@ -15,7 +14,7 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let cost = total_cost(pretend_user_input).unwrap_or(0);
 
     if cost > tokens {
         println!("You can't afford that many!");
@@ -25,6 +24,7 @@ fn main() {
     }
 }
 
+// since we use the ? it must return a Result<i32>, so just unwrap the result above
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -8,13 +8,20 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
+// use std::vec::Vec;
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut ans = self;
+        ans.push(String::from("Bar"));
+        ans
+    }
+}
 
 #[cfg(test)]
 mod tests {
