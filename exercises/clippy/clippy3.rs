0d6fbbba45562f53c7016b2290f92ebd692780f5
Daysgone <79004213+Days-gone@users.noreply.github.com>
2023-10-15 10:47:50 +0000

    try fitx clippy3

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -4,13 +4,16 @@
 //
 // Execute `rustlings hint clippy3` or use the `hint` watch subcommand for a hint.
 
+// I AM NOT DONE
+
 
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
     if my_option.is_none() {
         // let res = my_option.unwrap();
-        panic!("some thing wrong here!");
+        // panic!("some thing wrong here!");
+        
     }
 
     let my_arr = &[
@@ -19,7 +22,8 @@ fn main() {
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
-    let my_empty_vec = vec![1, 2, 3, 4, 5].clear();
+    let mut my_empty_vec = vec![1, 2, 3, 4, 5];
+    my_empty_vec.clear();
     println!("This Vec is empty, see? {:?}", my_empty_vec);
 
     let mut value_a = 45;
